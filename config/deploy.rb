9bff660708fb2af80bd53f2be4e606724a2d00cf
kaluznyo <kaluzny.olivier@gmail.com>
2017-09-28 21:09:57 +0200

    MAJ Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,9 @@
 lock "3.9.1"
 
 set :application, "sgs"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:kaluznyo/sgs_rails.git"
 
-set :deploy_to, '/home/deploy/my_app_name'
+set :deploy_to, '/home/deploy/sgs'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
